02c62dcf630f8825b985e160a53ea70e71c70d73
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-08-24 15:27:13 +0300

    commit tree 504e5ce00a416293c0845569e432ed0693e674eb

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,9 @@ require 'sqlite3'
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+
+# Student.create_table
+
+# user = Student.new("Ja", "K")
+# user.name
+# puts user
